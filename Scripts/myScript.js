e55faa451fedf9c5f53c90210e79fe54e3782311
TKahk <pedro.salgado@ua.pt>
2022-12-25 15:23:35 +0000

    countries

--- Scripts/myScript.js
+++ Scripts/myScript.js
@@ -3,16 +3,19 @@
         interval: 10000
     });
 });
-$(".Toggler").click(function() {
-    
+$(".Toggler").click(function () {
+
     $(this).children().toggleClass("fa-sun-o fa-moon-o");
     $(this).toggleClass("btn-dark");
     $(".navbar").toggleClass("navbar-dark bg-dark");
     $(".nav-items").toggleClass("nav-items-dark");
     $("body").toggleClass("dark-mode");
     $("table").toggleClass("nav-items-dark table-striped");
-    
+
 });
+
+
+
 
 
 
